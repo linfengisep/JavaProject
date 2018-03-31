c7c7e8b45f321e974e6549d0f7e8eeb535b780b4
linfengisep <wang.linfeng0213@gmail.com>
2018-03-31 17:05:12 +0200

    test date

--- head_first_java/UtilRandomNumber.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-public class UtilRandomNumber{
-   public static int ramdInt(int min,int max){
-      Random randomGenerator = new Random();
-      int randomNumber = randomGenerator.nextInt((max-min)+1)+min;
-      return randomNumber;
-   }
-}
