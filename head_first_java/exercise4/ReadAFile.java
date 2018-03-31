f7a5431b61fa4d7141735b8c836276d19234591f
linfengisep <wang.linfeng0213@gmail.com>
2018-03-31 22:31:32 +0200

    test Read file

---
+++ head_first_java/exercise4/ReadAFile.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class ReadAFile{
+   public static void main(String[]args){
+      try{
+         File myFile = new File("/Users/linfengwang/desktop/Java/JavaProject/head_first_java/exercise4/Game.ser");
+         FileReader fileReader = new FileReader(myFile);
+         BufferedReader reader = new BufferedReader(fileReader);
+         String line =null;
+         while((line = reader.readLine()) !=null){
+            System.out.println(line);
+         }
+      }catch(Exception ex){
+         ex.printStackTrace();
+      }
+   }
+}
