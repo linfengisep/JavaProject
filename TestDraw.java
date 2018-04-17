6a9921978281dae168c444fccfca8da9dbaa926c
linfengisep <wang.linfeng0213@gmail.com>
2018-04-17 23:49:04 +0200

    test draw with canvas and paint

---
+++ TestDraw.java
@@ -0,0 +1,37 @@
+public class TestDraw{
+   private void drawButtons(Canvas c, RecyclerView.ViewHolder viewHolder) {
+          float corners = 16;
+
+          View itemView = viewHolder.itemView;
+          Paint p = new Paint();
+         //this is the size of the button area; and it is a Rectangle;
+          RectF rightButton = new RectF(itemView.getRight() - buttonWidth, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+          p.setColor(Color.RED);
+          c.drawRoundRect(rightButton, corners, corners, p);
+          drawText("DELETE", c, rightButton, p);
+          drawCross(c,button,p);
+      }
+
+      private void drawText(String text, Canvas c, RectF button, Paint p) {
+          float textSize = 60;
+          p.setColor(Color.WHITE);
+          p.setAntiAlias(true);
+          p.setTextSize(textSize);
+
+          float textWidth = p.measureText(text);
+          c.drawText(text, button.centerX()-(textWidth/2), button.centerY()+(textSize/2), p);
+      }
+
+      private void drawCross(Canvas canvas,RectF button,Paint crossPaint){
+         //define the color of the line;
+         crossPaint.setColor(Color.WHITE);
+         //define the start point and stop point of the line;
+        //float width = button.width();
+        //float height = button.height();
+         //the cross rectangle  left,top,right,bottom; width = height = 60;
+         //RectF crossRectangle = new RectF(button.centerX - 30,button.centerY + 60+20,button.centerX + 30,button.centerY +20);
+         //canvas.drawRect(crossRectangle,crossPaint);
+         canvas.drawLine(button.centerX -30,button.centerY + 20,button.centerX+30,button.centerY + 20+60);
+         canvas.drawLine(button.centerX -30,button.centerY + 20+60,button.centerX+30,button.centerY + 20);
+      }
+}
