d98fd47791d7070c06661a39eb411390fec60ff4
linfengisep <wang.linfeng0213@gmail.com>
2018-11-14 14:30:14 +0100

    today i add yes in file

--- TestA.java
+++ TestA.java
@@ -1,5 +1,11 @@
 public class TestA{
    public static void main(String[] args) {
    System.out.println("ok");
+System.out.println("yes");
+System.out.println("yes");
+System.out.println("yes");
+System.out.println("yes");
+System.out.println("yes");
+System.out.println("yes");
  }
 }
